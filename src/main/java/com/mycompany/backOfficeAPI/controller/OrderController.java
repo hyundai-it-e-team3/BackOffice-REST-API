9c8ce5a959fa22f7191a942c93478a8773e3610a
Chenjae <injea95@naver.com>
2021-12-10 10:37:13 +0900

    HYUN-203 <주문상태 변경 구현>

--- src/main/java/com/mycompany/backOfficeAPI/controller/OrderController.java
+++ src/main/java/com/mycompany/backOfficeAPI/controller/OrderController.java
@@ -8,6 +8,7 @@ import javax.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -82,8 +83,8 @@ public class OrderController{
 		return orderService.getOrderDetail(orderId);
 	}
 	
-	@PatchMapping("/detail")
-	public void updateState(@RequestBody OrderDetail orderDetail) {
+	@PostMapping("/detail")
+	public void updateState(OrderDetail orderDetail) {
 		log.info("실행");
 		log.info("orderDetail : " + orderDetail);
 		
